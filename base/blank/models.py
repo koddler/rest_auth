dea07a1324fe3880876ae3902eb13a15ccd09dce
Shaphil <shaphil2025@gmail.com>
2018-09-12 13:10:59 +0600

    Customize Django user model

---
+++ base/blank/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import (
+    check_password, make_password,
+)
+from django.utils import timezone
+from django.utils.crypto import get_random_string, salted_hmac
+
+
+class AbstractBaseUser(models.Model):
+    password = models.CharField('password', max_length=128)
+    last_login = models.DateTimeField('last login', blank=True, null=True)
+
+    # Stores the raw password if set_password() is called so that it can
+    # be passed to password_changed() after the model is saved.
+    _password = None
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.set_password(self.password)
+        super().save(*args, **kwargs)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self._password = raw_password
+
+    def check_password(self, raw_password):
+        """
+        Return a boolean of whether the raw_password was correct. Handles
+        hashing formats behind the scenes.
+        """
+        def setter(raw_password):
+            self.set_password(raw_password)
+            # Password hash upgrades shouldn't be considered password changes.
+            self._password = None
+            self.save(update_fields=["password"])
+        return check_password(raw_password, self.password, setter)
+
+    def get_session_auth_hash(self):
+        """
+        Return an HMAC of the password field.
+        """
+        key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash"
+        return salted_hmac(key_salt, self.password).hexdigest()
+
+
+class User(AbstractBaseUser):
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(blank=True)
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text='Designates whether this user should be treated as active. '
+        'Unselect this instead of deleting accounts.',
+    )
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
