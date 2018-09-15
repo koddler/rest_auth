7f0623d5a765a1e4843086c28c582ef7b2b21367
Shaphil <shaphil2025@gmail.com>
2018-09-15 19:33:27 +0600

    Add modified Token model from DRF

--- base/blank/models.py
+++ base/blank/models.py
@@ -1,3 +1,6 @@
+import binascii
+import os
+
 from django.db import models
 from django.contrib.auth import password_validation
 from django.contrib.auth.hashers import (
@@ -69,3 +72,26 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+
+
+class Token(models.Model):
+    """
+    The default authorization token model.
+    """
+    key = models.CharField(max_length=40, primary_key=True)
+    user = models.OneToOneField(
+        User, related_name='auth_token',
+        on_delete=models.CASCADE, verbose_name="User"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(Token, self).save(*args, **kwargs)
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __str__(self):
+        return self.key
