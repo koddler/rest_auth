bf7d38c55d244696d627df6e1f9cade48a1467ac
Shaphil <shaphil2025@gmail.com>
2018-09-15 19:34:28 +0600

    Add LoginView, RegistrationView; Update urls
    * Add RegistrationView: data is not validated
    * Add LoginView that takes in a username/password combination and
      returns a token on successful login.
    * Add url routes for registration and login.

--- base/blank/urls.py
+++ base/blank/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import LoginView, RegistrationView, UserViewSet
 
 router = routers.DefaultRouter()
 router.register('user', UserViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('login/', LoginView.as_view()),
+    path('register/', RegistrationView.as_view())
 ]
--- base/blank/views.py
+++ base/blank/views.py
@@ -1,9 +1,54 @@
-from rest_framework import viewsets
+import json
 
-from .models import User
+from rest_framework import views, viewsets
+from rest_framework.response import Response
+
+from .models import Token, User
 from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class RegistrationView(views.APIView):
+    def post(self, request):
+        user = User(
+            username=request.data['username'],
+            password=request.data['password']
+        )
+        user.save()
+
+        return Response(
+            json.dumps({"message": "created"}),
+            status=200
+        )
+
+
+class LoginView(views.APIView):
+    def post(self, request):
+        try:
+            user = User.objects.get(username=request.data['username'])
+        except User.DoesNotExist:
+            return Response(
+                json.dumps({'message': 'User does not exist'}),
+                status=401
+            )
+
+        password = request.data['password']
+        is_correct_password = user.check_password(password)
+
+        if(is_correct_password):
+            token = Token()
+            token.user = user
+            token.save()
+            return Response(
+                json.dumps({'token': token.key}),
+                status=202
+            )
+
+        return Response(
+            json.dumps({'error': 'Invalid password'}),
+            status=401
+        )
